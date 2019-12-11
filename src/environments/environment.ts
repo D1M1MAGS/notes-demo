ac68405ee85cc29936460142654ea6e6a2d85bd4
D1M1MAGOO <dimimagoo2015@gmail.com>
2019-12-11 14:22:42 +1100

    commit first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDoO8jl2tTV2STvmtNLRcdNVlNS7ntQA6A",
+    authDomain: "notes-demo-11833.firebaseapp.com",
+    databaseURL: "https://notes-demo-11833.firebaseio.com",
+    projectId: "notes-demo-11833",
+    storageBucket: "notes-demo-11833.appspot.com",
+    messagingSenderId: "776362097362",
+    appId: "1:776362097362:web:2173d52677b06a07fa4b0b",
+    measurementId: "G-KWLFP8VTW8"
+  }
 };
 
 /*
